dc34424fa6ccbd4532d27611a690b8114e91741a
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-01-25 16:10:24 -0300

    adds routes to controller actions and resolves CSFR Token error

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,4 +3,6 @@ class UrlsController < ApplicationController
         @short_url = Url.create(original_url: params[:original_url], short_url: SecureRandom.hex(5))
         render json: { original_url: @short_url.original_url, short_url: @short_url.short_url }
     end
+
+    
 end
