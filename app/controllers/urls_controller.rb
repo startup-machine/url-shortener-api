caf71befe342cc27c29db086c86539aa4623216f
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-01-25 16:03:01 -0300

    implements the URL shortening logic in the create controller action

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,6 @@
+class UrlsController < ApplicationController
+    def create
+        @short_url = Url.create(original_url: params[:original_url], short_url: SecureRandom.hex(5))
+        render json: { original_url: @short_url.original_url, short_url: @short_url.short_url }
+    end
+end
